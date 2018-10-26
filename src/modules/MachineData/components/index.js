976651bf937411a3344ac84b6de7764bfab1a87a
Alan Albertengo <alan.albertengo@santexgroup.com>
2018-10-26 13:09:03 -0300

    Added editing pluging

--- src/modules/MachineData/components/index.js
+++ src/modules/MachineData/components/index.js
@@ -1,7 +1,15 @@
 import React, { PureComponent } from 'react';
-import '@devexpress/dx-react-grid'; // State or Local Processing Plugins
-import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-bootstrap4';
-import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
+import { Card } from 'reactstrap';
+import { EditingState } from '@devexpress/dx-react-grid';
+import {
+  Grid,
+  Table,
+  TableHeaderRow,
+  TableEditRow,
+  TableEditColumn,
+} from '@devexpress/dx-react-grid-bootstrap4';
+
+const getRowId = row => row.id;
 
 class MachineDataSection extends PureComponent {
   constructor(props) {
@@ -15,32 +23,47 @@ class MachineDataSection extends PureComponent {
         { name: 'car', title: 'Car' },
       ],
       rows: [
-        { sex: 'Female', name: 'Sandra', city: 'Las Vegas', car: 'Audi A4' },
-        { sex: 'Male', name: 'Paul', city: 'Paris', car: 'Nissan Altima' },
-        { sex: 'Male', name: 'Mark', city: 'Paris', car: 'Honda Accord' },
-        { sex: 'Male', name: 'Paul', city: 'Paris', car: 'Nissan Altima' },
-        { sex: 'Female', name: 'Linda', city: 'Austin', car: 'Toyota Corolla' },
-        { sex: 'Male', name: 'Robert', city: 'Las Vegas', car: 'Chevrolet Cruze' },
-        { sex: 'Female', name: 'Lisa', city: 'London', car: 'BMW 750' },
-        { sex: 'Male', name: 'Mark', city: 'Chicago', car: 'Toyota Corolla' },
-        { sex: 'Male', name: 'Thomas', city: 'Rio de Janeiro', car: 'Honda Accord' },
-        { sex: 'Male', name: 'Robert', city: 'Las Vegas', car: 'Honda Civic' },
-        { sex: 'Female', name: 'Betty', city: 'Paris', car: 'Honda Civic' },
-        { sex: 'Male', name: 'Robert', city: 'Los Angeles', car: 'Honda Accord' },
-        { sex: 'Male', name: 'William', city: 'Los Angeles', car: 'Honda Civic' },
-        { sex: 'Male', name: 'Mark', city: 'Austin', car: 'Nissan Altima' },
+        { id: 1, sex: 'Female', name: 'Sandra', city: 'Las Vegas', car: 'Audi A4' },
+        { id: 2, sex: 'Male', name: 'Paul', city: 'Paris', car: 'Nissan Altima' },
       ],
     };
   }
+
+  commitChanges = ({ added, changed, deleted }) => {
+    let { rows } = this.state;
+    if (added) {
+      const startingAddedId = rows.length > 0 ? rows[rows.length - 1].id + 1 : 0;
+      rows = [
+        ...rows,
+        ...added.map((row, index) => ({
+          id: startingAddedId + index,
+          ...row,
+        })),
+      ];
+    }
+    if (changed) {
+      rows = rows.map(row => (changed[row.id] ? { ...row, ...changed[row.id] } : row));
+    }
+    if (deleted) {
+      const deletedSet = new Set(deleted);
+      rows = rows.filter(row => !deletedSet.has(row.id));
+    }
+    this.setState({ rows });
+  };
 
   render() {
     const { rows, columns } = this.state;
 
     return (
-      <Grid rows={rows} columns={columns}>
-        <Table />
-        <TableHeaderRow />
-      </Grid>
+      <Card>
+        <Grid rows={rows} columns={columns} getRowId={getRowId}>
+          <EditingState onCommitChanges={this.commitChanges} />
+          <Table />
+          <TableHeaderRow />
+          <TableEditRow />
+          <TableEditColumn showAddCommand showEditCommand showDeleteCommand />
+        </Grid>
+      </Card>
     );
   }
 }
